362e1226f1d772db26933a074cdc0fc2bc45134a
Ian MacDonald & Kim Dang <student@epicodus.com>
2015-06-08 11:41:46 -0700

    Add links to headlines on homepage.

--- router.js
+++ router.js
@@ -1,3 +1,9 @@
 Bustle.Router.map(function() {
   this.resource('headline', {path: '/'});
+  this.resource('vacations', function() {
+    this.resource('1');
+    this.resource('2');
+  });
+  this.resource('food');
+  this.resource('sports');
 });
